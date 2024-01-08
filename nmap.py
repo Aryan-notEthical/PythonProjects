92c73588f8e0912a0054a8dd84e6c5281de9ebbf
Aryan <103557267+Aryan-notEthical@users.noreply.github.com>
2024-01-08 19:15:10 +0530

    Add files via upload

---
+++ nmap.py
@@ -0,0 +1,19 @@
+import os
+
+
+banner = ''' 
+
+Made By (instagram)  @aryan_not_ethical
+
+WARNING do not use it if your are not having a vpn
+this will only work it you have nmap installed on your system
+every thing is faltu here
+
+'''
+
+
+print(banner)
+input = input("Enter the url/ip -->")
+print("-----------------------------")
+os.system("nmap "+ input)
+print("-----------------------------") 
